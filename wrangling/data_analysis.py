260663089821d017d0025f81ae583c53abcba978
Milla Peltoniemi <tunturikissa@gmail.com>
2018-04-18 12:11:18 +0300

    remove tabs from the dataset

--- wrangling/data_analysis.py
+++ wrangling/data_analysis.py
@@ -5,5 +5,5 @@ import pandas as pd
 fertility_rate = pd.read_csv("../data/kokonaishedelmallisyys_2000_2016.csv", encoding='utf8')
 births = pd.read_csv("../data/syntyneet_lapset_1987_2016.csv", encoding='utf8')
 
-pd.head(fertility_rate)
-pd.head(births)
+print(fertility_rate.head())
+print(births.head())
