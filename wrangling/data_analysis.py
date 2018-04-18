ceffec8b45eb098e7548f11759659f090a9afdf7
Milla Peltoniemi <tunturikissa@gmail.com>
2018-04-18 12:55:31 +0300

    quick overview how much birthrate has changed from 2015 to 2016

--- wrangling/data_analysis.py
+++ wrangling/data_analysis.py
@@ -5,5 +5,20 @@ import pandas as pd
 fertility_rate = pd.read_csv("../data/kokonaishedelmallisyys_2000_2016.csv", encoding='utf8', sep=';')
 births = pd.read_csv("../data/syntyneet_lapset_1987_2016.csv", encoding='utf8', sep=';')
 
-print(fertility_rate.head())
-print(births.head())
+#print(fertility_rate.head())
+#print(births.head())
+#print(births.columns.values)
+
+births2016col = '2016 Sukupuolet yhteensä Elävänä syntyneet, lkm'
+births2015col = '2015 Sukupuolet yhteensä Elävänä syntyneet, lkm'
+births = births.loc[births['Äidin ikä'] == "Ikäluokat yhteensä"]
+births15and16 = births[['Alue', births2015col, births2016col]]
+print(births15and16.head())
+
+births15and16['change'] = ((births15and16[births2016col]-births15and16[births2015col])/births15and16[births2016col])*100
+births15and16['change'].round(2)
+births15and16 = births15and16.sort_values(['change'], ascending=True)
+print(births15and16.head())
+
+births15and16 = births15and16.sort_values([births2015col], ascending=False)
+print(births15and16.head())
