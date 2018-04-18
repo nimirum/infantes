e671fc88e6b41b8e22663a83f608d0259fdeccc4
Milla Peltoniemi <tunturikissa@gmail.com>
2018-04-18 12:19:11 +0300

    add seperator to read csv

--- wrangling/data_analysis.py
+++ wrangling/data_analysis.py
@@ -2,8 +2,8 @@
 import pandas as pd
 
 ##
-fertility_rate = pd.read_csv("../data/kokonaishedelmallisyys_2000_2016.csv", encoding='utf8')
-births = pd.read_csv("../data/syntyneet_lapset_1987_2016.csv", encoding='utf8')
+fertility_rate = pd.read_csv("../data/kokonaishedelmallisyys_2000_2016.csv", encoding='utf8', sep=';')
+births = pd.read_csv("../data/syntyneet_lapset_1987_2016.csv", encoding='utf8', sep=';')
 
 print(fertility_rate.head())
 print(births.head())
