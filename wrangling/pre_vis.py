45f4f4b221541156144345de5fbe9a3b81e3737d
Milla Peltoniemi <tunturikissa@gmail.com>
2018-04-25 12:54:18 +0300

    Add municipalities values to dataframe

--- wrangling/pre_vis.py
+++ wrangling/pre_vis.py
@@ -7,22 +7,27 @@ from plotly.graph_objs import Layout
 
 def plot_test():
     df = get_data()
-    df = df[df['Alue'] != 'KOKO MAA']
-    vakiluvut = get_kuntadata()
-    df = pd.merge(right=vakiluvut, left=df, how='left', on='Alue')
-    df['syntvaki'] = (df['2016 Sukupuolet yhteensä Elävänä syntyneet, lkm'] /df['2016']) * 100
+    df = df[df['Area'] != 'KOKO MAA']
+    municipalities = get_kuntadata()
+    df = pd.merge(right=municipalities, left=df, how='left', on='Area')
+   # print(df.head())
+
+    df['syntvaki'] = df['Births2016'] / df['Population'] * 100
     df = df.sort_values(by='syntvaki')
     print(df.head())
+    df2 = df[['Area', 'syntvaki', 'Population', 'Births2016', 'Unemployment', 'UniversityDegree', 'BirthDeathSum']]
+    df2.columns = ['Area', 'Births population ratio', 'Population', 'Births', 'Unemployment', 'UniversityDegree', 'BirthDeathSum']
+    df2.to_json('births_with_population.json', orient='records', force_ascii=False)
+    print(df2.head())
 
-
-    init_notebook_mode(connected=False)
-    data = [go.Bar(
-        y=df['syntvaki'],
-        x=df['Alue'],
-    )]
-    layout = go.Layout(title="Percentage of newborns per population")
-    fig = go.Figure(data=data, layout=layout)
-    offline.plot(fig)
+    #init_notebook_mode(connected=False)
+    # data = [go.Bar(
+    #    y=df['syntvaki'],
+    #    x=df['Alue'],
+    # )]
+    #layout = go.Layout(title="Percentage of newborns per population")
+    #fig = go.Figure(data=data, layout=layout)
+    #offline.plot(fig)
     '''
     plot({
         "data": data,
@@ -41,21 +46,47 @@ def get_data():
     births15and16 = births[['Alue', births2015col, births2016col]]
     births15and16 = births15and16.reset_index()
 
-    print(births15and16.head())
+    #print(births15and16.head())
 
     births15and16['change'] = ((births15and16[births2016col] - births15and16[births2015col]) / births15and16[
         births2016col]) * 100
     births15and16['change'].round(2)
     births15and16 = births15and16.sort_values(['change'], ascending=True)
+    births15and16.columns = ['index', 'Area', 'Births2015', 'Births2016', 'change']
+    del births15and16['index']
     print(births15and16.head())
     return births15and16
+
+
 def get_kuntadata():
     keys = pd.read_csv("../data/kuntien_avainluvut_2000_2017.csv", encoding='utf8', sep=';')
     keys['Alue'] = keys['Alue 2017']
-    keys = keys[(keys['Tiedot'] == 'Väkiluku') & (keys['Alue'] != 'KOKO MAA')][['2016', 'Alue']].reset_index()
+    keys1 = keys[((keys['Tiedot'] == ('Väkiluku')))
+                & (keys['Alue'] != 'KOKO MAA')][['2016', 'Alue']].reset_index()
+    keys1.columns = ['index', 'Population', 'Area']
+    del keys1['index']
 
-    print(keys.head())
+    keys2 = keys[( (keys['Tiedot'] == ('Syntyneiden enemmyys, henkilöä')))
+                & (keys['Alue'] != 'KOKO MAA')][['2016', 'Alue']].reset_index()
+    keys2.columns = ['index', 'BirthDeathSum', 'Area']
+    del keys2['index']
+
+    keys3 = keys[((keys['Tiedot'] == ('Korkea-asteen tutkinnon suorittaneiden osuus 15 vuotta täyttäneistä, %')))
+                & (keys['Alue'] != 'KOKO MAA')][['2016', 'Alue']].reset_index()
+    keys3.columns = ['index', 'UniversityDegree', 'Area']
+    del keys3['index']
+
+    keys4 = keys[((keys['Tiedot'] == ('Työttömien osuus työvoimasta, %')))
+                & (keys['Alue'] != 'KOKO MAA')][['2016', 'Alue']].reset_index()
+    keys4.columns = ['index', 'Unemployment', 'Area']
+    del keys4['index']
+
+    keys = pd.merge(right=keys1, left=keys2, how='left', on='Area')
+    keys = pd.merge(right=keys, left=keys3, how='left', on='Area')
+    keys = pd.merge(right=keys, left=keys4, how='left', on='Area')
+    #print(keys.head())
     return keys
+
 
 if __name__ == "__main__":
     #get_kuntadata()
