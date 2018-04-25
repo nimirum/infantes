5ae729bb880d7794ebc3f8bb3e6d59ef68bc2a47
Milla Peltoniemi <tunturikissa@gmail.com>
2018-04-25 15:43:48 +0300

    replace nan and inf values with 0

--- wrangling/pre_vis.py
+++ wrangling/pre_vis.py
@@ -3,6 +3,7 @@ from plotly.offline import init_notebook_mode, plot
 import plotly.graph_objs as go
 import plotly.offline as offline
 from plotly.graph_objs import Layout
+import numpy as np
 
 
 def plot_test():
@@ -14,31 +15,33 @@ def plot_test():
 
     df['syntvaki'] = df['Births2016'] / df['Population'] * 100
     df = df.sort_values(by='syntvaki')
-    print(df.head())
+    #print(df.head())
     df2 = df[['Area', 'Region', 'syntvaki', 'Population', 'Births2016', 'change', 'Unemployment', 'UniversityDegree', 'BirthDeathSum']]
-    df2.columns = ['Area', 'Region', 'Births population ratio', 'Population', 'Births2016', 'Births change %', 'Unemployment', 'UniversityDegree', 'BirthDeathSum']
+    df2.columns = ['Area', 'Region', 'BirthsPopulationRatio', 'Population', 'Births2016', 'BirthsChange%', 'Unemployment', 'UniversityDegree', 'BirthDeathSum']
+    df2.fillna(0, inplace=True)
+    df2.replace(-np.inf, 0, inplace=True)
     df2.to_json('births_with_population.json', orient='records', force_ascii=True)
     print(df2.head())
 
-    init_notebook_mode(connected=False)
-    data = [go.Scatter(
-        y=df2['Births population ratio'],
-        x=df2['Unemployment'],
-        mode='markers',
-        marker=dict(
-            color='#FFBAD2'
-        )
-     )]
+    #init_notebook_mode(connected=False)
+    #data = [go.Scatter(
+    #    y=df2['Births2016'],
+    #    x=df2['Population'],
+    #    hoverinfo=df2['Area'],
+    #    mode='markers',
+    # )]
 
-    layout = go.Layout(title="Percentage of newborns per population")
-    fig = go.Figure(data=data, layout=layout)
-    offline.plot(fig)
+    #layout = go.Layout(title="Percentage of newborns per population and university degree")
+    #fig = go.Figure(data=data, layout=layout)
+    #offline.plot(fig)
+
     '''
     plot({
         "data": data,
         "layout": Layout(title="Testplot")
     }, image='png', image_filename="testplot")
     '''
+    print(df2['BirthsChange%'].unique())
 
 
 def get_data():
@@ -59,6 +62,9 @@ def get_data():
     births15and16 = births15and16.sort_values(['change'], ascending=True)
     births15and16.columns = ['index', 'Area', 'Births2015', 'Births2016', 'change']
     del births15and16['index']
+
+    births15and16['Area'].replace('Maarianhamina - Mariehamn', 'Maarianhamina', inplace=True)
+
     print(births15and16.head())
     return births15and16
 
