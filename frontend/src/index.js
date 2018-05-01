c156c63f738935ad11afe4c46781e52f8871ffa2
Johannes Elmnäinen <johannes.elmnainen@cs.helsinki.fi>
2018-05-01 22:34:57 +0300

    X axis can be changed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -33,15 +33,17 @@ const xAttributes = [
 ]
 
 const attributesToString = {
-	'BirthsPopulationRatio': 'Births to population',
 	'Unemployment': 'Unemployment',
+	'MenUnemployed2016': "Men unemployed in 2016",
+	'WomenUnemployed2016': 'Women unemployed in 2016',
 	'Population': 'Population',
+	'UniversityDegree': '% with university degree',
+	'BirthsPopulationRatio': 'Births to population'
 }
 
 const initApp = () => {
 	const scales = Scatterplot.scales(state, width, height)
 	state = R.merge(state, scales)
-	console.log(state)
 	const svg = Scatterplot.renderBase('#visualization', margin, width, height, state, attributesToString)
 	state = R.merge(state, {svg})
 	Scatterplot.renderBubbles(state)
@@ -50,19 +52,17 @@ const initApp = () => {
 	console.log(state)
 }
 const updateState = (newState) => {
+	const newScales = Scatterplot.scales(state, width, height)
 	state = R.merge(state, newState)
+	Scatterplot.redrawXAxis(state, attributesToString)
 	Scatterplot.renderBubbles(state)
+	Scatterplot.renderLegend(state, width)
 	console.log(state)
 }
 
-window.onButtonClick = (e) => {
-	const index = Math.floor(state.data.length / 2)
-	const data = state.data.slice(index)
-	updateState({data})
-}
-
 const handleXSelectChange = (val) => {
-	updateState({'xAttribute': val})
+	const xAxis = Scatterplot.updateXdim(state, val)
+	updateState({'xAttribute': val, xAxis})
 }
 
 initApp()
--- frontend/src/select.js
+++ frontend/src/select.js
@@ -1,6 +1,5 @@
 const createSelect = (props, targetElementId) => {
   window.handleSelectChange = props.handleSelectChange
-  const container = document.createElement('div')
   const select  = document.createElement('select')
   const options = props.options ? props.options : []
   options.map((opt) => {
@@ -9,10 +8,10 @@ const createSelect = (props, targetElementId) => {
     option.appendChild(document.createTextNode(opt))
     select.appendChild(option)
   })
+  select.setAttribute('class', 'custom-select')
   select.setAttribute('onchange', 'handleSelectChange(value)')
   const targetElement = document.getElementById(targetElementId)
-  container.appendChild(select)
-  targetElement.appendChild(container)
+  targetElement.appendChild(select)
 }
 
 
